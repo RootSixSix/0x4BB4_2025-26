e08e51c3fe2ec2f7f377d1316a9053113faf843e
RootSixSix <59634583+RootSixSix@users.noreply.github.com>
2025-04-19 15:24:38 -0500

    Third commit.   This will be the first version I try to Build on the 'bot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_2025.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_2025.java
@@ -36,99 +36,78 @@ for a description of the differences between LinearOpMode and Opmode.
 @TeleOp(name="Teleop Dev01", group="19380")
 public class Robot_2025 extends LinearOpMode {
 
-    private static class DrivePowers {
+    // These are the current power setting for each of the 4 drive motors.
+    double leftFront = 0;  // Current power for the robot's left front drive motor
+    double leftRear = 0;  // Current power for the robot's left rear drive motor
+    double rightFront = 0;  // Current power for the robot's right front drive motor
+    double rightRear = 0;  // Current power for the robot's right rear drive motor
+
+    // Method to read the sticks on gamepad 1 and update the drive motors' power
+    public void updateDrivePowers(Gamepad gamepad1) {
 
         // Declare constants
-        static double ROTATION_SCALE = 0.75;   // Scaling factor to determine "twisting" effect on power levels
-        static double MAX_POWER_INCREASE = 0.05;   // Maximum power increase (acceleration) per update
-        static double MAX_POWER_DECREASE = -0.05;   // Maximum power decrease (deceleration) per update
+        double ROTATION_SCALE = 0.75;       // Scaling factor to determine "twisting" effect on power levels
+        double MAX_POWER_INCREASE = 0.05;   // Maximum power increase (acceleration) per update
+        double MAX_POWER_DECREASE = -0.05;  // Maximum power decrease (deceleration) per update
 
-        double currentPowerLF;  // Current power for the robot's left front drive motor
-        double currentPowerLR;  // Current power for the robot's left rear drive motor
-        double currentPowerRF;  // Current power for the robot's right front drive motor
-        double currentPowerRR;  // Current power for the robot's right rear drive motor
+        // The driver controls the robot using:
+        //  1.  Linear motion using the left stick on controller 1 with "drive" (forward/backward) and
+        //      "strafe" (left/right), and
+        //  2.  Rotational motion using the right stick on controller 1 with "twist" (rotation).
+        //
+        // Read the left and right sticks of Gamepad 1.  The deadStick method returns 0 if
+        // -0.05 >= value of stick <= 0.05.
+        double drive = UtilsLib.deadStick(gamepad1.left_stick_y);
+        double strafe = UtilsLib.deadStick(gamepad1.left_stick_x);
+        double twist = UtilsLib.deadStick(gamepad1.right_stick_x);
 
-        // Class constructor
-        private DrivePower() {
+        // Calculate the difference between the current power levels and the target power levels calculated
+        // from sticks on gamepad1. These can be positive (acceleration) or negative (deceleration).
+        double deltaPowerLF = leftFront - (drive + strafe + (ROTATION_SCALE * twist));
+        double deltaPowerRF = rightFront - (drive - strafe - (ROTATION_SCALE * twist));
+        double deltaPowerLR = leftRear - (drive - strafe + (ROTATION_SCALE * twist));
+        double deltaPowerRR = rightRear - (drive + strafe - (ROTATION_SCALE * twist));
 
-            currentPowerLF = 0;  // Initialize current power for the robot's left front drive motor
-            currentPowerLR = 0;  // Initialize current power for the robot's left rear drive motor
-            currentPowerRF = 0;  // Initialize current power for the robot's right front drive motor
-            currentPowerRR = 0;  // Initialize current power for the robot's right rear drive motor
-
+        // If delta > 0 (acceleration), add the lesser of the delta and the maximum power increase.
+        // If delta <= 0 (deceleration), add the greater of the delta and the maximum power decrease
+        // (we "add the greater of" because both delta <=0 and MAX_POWER_DECREASE < 0).
+        if (deltaPowerLF > 0) {
+            leftFront += Math.min(MAX_POWER_INCREASE, deltaPowerLF);
+        } else {
+            leftFront += Math.max(MAX_POWER_DECREASE, deltaPowerLF);
         }
-        // Method to read the sticks on controller 1 and update the drive motors' power
-        private void updateDrivePower(Gamepad gamepad1) {
+        if (deltaPowerRF > 0) {
+            rightFront += Math.min(MAX_POWER_INCREASE, deltaPowerRF);
+        } else {
+            rightFront += Math.max(MAX_POWER_DECREASE, deltaPowerRF);
+        }
+        if (deltaPowerLR > 0) {
+            leftRear += Math.min(MAX_POWER_INCREASE, deltaPowerLR);
+        } else {
+            leftRear += Math.max(MAX_POWER_DECREASE, deltaPowerLR);
+        }
+        if (deltaPowerRR > 0) {
+            rightRear += Math.min(MAX_POWER_INCREASE, deltaPowerRR);
+        } else {
+            rightRear += Math.max(MAX_POWER_DECREASE, deltaPowerRR);
+        }
 
-            // The driver controls the robot using:
-            //  1.  Linear motion using the left stick on controller 1 with "drive" (forward/backward) and
-            //      "strafe" (left/right), and
-            //  2.  Rotational motion using the right stick on controller 1 with "twist" (rotation).
-            //
-            // Read the left and right sticks of Gamepad 1.
-            double drive = UtilsLib.deadStick(gamepad1.left_stick_y);
-            double strafe = UtilsLib.deadStick(gamepad1.left_stick_x);
-            double twist = UtilsLib.deadStick(gamepad1.right_stick_x);
+        // Determine the maximum target power level among the drive motors so we can normalize
+        // the power levels if one or more of them is greater than 1.
+        double max = Math.max(Math.abs(leftFront), Math.abs(rightFront));
+        max = Math.max(max, Math.abs(leftRear));
+        max = Math.max(max, Math.abs(rightRear));
 
-            // Calculate the difference between the current power levels and the target power levels calculated
-            // from sticks on gamepad1. These can be positive (acceleration) or negative (deceleration).
-            double deltaPowerLF = currentPowerLF - (drive + strafe + (ROTATION_SCALE * twist));
-            double deltaPowerRF = currentPowerRF - (drive - strafe - (ROTATION_SCALE * twist));
-            double deltaPowerLR = currentPowerLR - (drive - strafe + (ROTATION_SCALE * twist));
-            double deltaPowerRR = currentPowerRR - (drive + strafe - (ROTATION_SCALE * twist));
-
-            // If delta > 0 (acceleration), add the lesser of the delta and the maximum power increase.
-            // If delta <= 0 (deceleration), add the greater of the delta and the maximum power decrease
-            // (we "add the greater of" because both delta <=0 and MAX_POWER_DECREASE < 0).
-            if (deltaPowerLF > 0) {
-                currentPowerLF += Math.min(MAX_POWER_INCREASE, deltaPowerLF);
-            } else {
-                currentPowerLF += Math.max(MAX_POWER_DECREASE, deltaPowerLF);
-            }
-            if (deltaPowerRF > 0) {
-                currentPowerRF += Math.min(MAX_POWER_INCREASE, deltaPowerRF);
-            } else {
-                currentPowerRF += Math.max(MAX_POWER_DECREASE, deltaPowerRF);
-            }
-            if (deltaPowerLR > 0) {
-                currentPowerLR += Math.min(MAX_POWER_INCREASE, deltaPowerLR);
-            } else {
-                currentPowerLR += Math.max(MAX_POWER_DECREASE, deltaPowerLR);
-            }
-            if (deltaPowerRR > 0) {
-                currentPowerRR += Math.min(MAX_POWER_INCREASE, deltaPowerRR);
-            } else {
-                currentPowerRR += Math.max(MAX_POWER_DECREASE, deltaPowerRR);
-            }
-
-            // Determine the maximum target power level among the drive motors so we can normalize
-            // the power levels if one or more of them is greater than 1.
-            double max = Math.max(Math.abs(currentPowerLF), Math.abs(currentPowerRF));
-            max = Math.max(max, Math.abs(currentPowerLR));
-            max = Math.max(max, Math.abs(currentPowerRR));
-
-            // If the maximum power level for any motor is greater than 1, then normalize the power
-            // levels so the maximum power level is 1.
-            if (max > 1.0) {
-                currentPowerLF  /= max;
-                currentPowerRF /= max;
-                currentPowerLR   /= max;
-                currentPowerRR  /= max;
-            }
-
-
-
-
+        // If the maximum power level for any motor is greater than 1, then normalize the power
+        // levels so the maximum power level is 1.
+        if (max > 1.0) {
+            leftFront  /= max;
+            rightFront /= max;
+            leftRear   /= max;
+            rightRear  /= max;
         }
 
     }
-
-    // Declare and initialize DcMotors.
-    private DcMotor front_left = null;  // Front left drive motor
-    private DcMotor front_right = null;  // Front right drive motor
-    private DcMotor back_left = null;  // Back left drive motor
-    private DcMotor back_right = null;  // Back right drive motor
-
 
     // runOpMode() runs exactly once when the INIT button is pressed.
     // All code for the OpMode goes in here.
@@ -136,10 +115,11 @@ public class Robot_2025 extends LinearOpMode {
     public void runOpMode() {
 
         // Name strings must match the config names on the Robot Controller app.
-        front_left = hardwareMap.get(DcMotor.class, "fldrive");
-        front_right = hardwareMap.get(DcMotor.class, "frdrive");
-        back_left = hardwareMap.get(DcMotor.class, "bldrive");
-        back_right = hardwareMap.get(DcMotor.class, "brdrive");
+        // Declare and initialize DcMotors.
+        DcMotor front_left = hardwareMap.get(DcMotor.class, "fldrive");
+        DcMotor front_right = hardwareMap.get(DcMotor.class, "frdrive");
+        DcMotor back_left = hardwareMap.get(DcMotor.class, "bldrive");// Back right drive motor
+        DcMotor back_right = hardwareMap.get(DcMotor.class, "brdrive");
 
         // Left and right motor must turn in opposite directions because they
         // have mirror symmetry.
@@ -148,17 +128,16 @@ public class Robot_2025 extends LinearOpMode {
         front_right.setDirection(DcMotor.Direction.FORWARD);
         back_right.setDirection(DcMotor.Direction.FORWARD);
 
+        // Declare and initialize current and previous gamepad objects.  We use current
+        // and previou objects so that we can detect changes in the state of the gamepads
+        // during the "while (opModeIsActive()) {}" loop. For example, if the current state
+        // of gamepad1 is that the A button is pressed and the previous state is that the
+        // A button is not pressed, then we know that the driver pressed the A button during
+        // last loop.  See https://gm0.org/en/latest/docs/software/tutorials/gamepad.html
         Gamepad currentGamepad1 = new Gamepad();
         Gamepad currentGamepad2 = new Gamepad();
-
         Gamepad previousGamepad1 = new Gamepad();
         Gamepad previousGamepad2 = new Gamepad();
-
-        // These are the current power setting for each of the 4 drive motors.
-        double currentLeftFrontPower = 0;
-        double currentRightFrontPower = 0;
-        double currentLeftBackPower = 0;
-        double currentRightBackPower = 0;
 
         /* Wait for the game driver to press play */
         waitForStart();
@@ -166,11 +145,19 @@ public class Robot_2025 extends LinearOpMode {
         /* Run until the driver presses stop */
         while (opModeIsActive()) {
 
-            // Set the motors to the calculated values.
-            front_left.setPower(currentLeftFrontPower);
-            front_right.setPower(currentRightFrontPower);
-            back_left.setPower(currentLeftBackPower);
-            back_right.setPower(currentRightBackPower);
+            // Store the currentGamepad1/2 values (which were used last loop iteration) in
+            // previousGamepad1/2 so we can detect state changes for this iteration.
+            previousGamepad1.copy(currentGamepad1);
+            previousGamepad2.copy(currentGamepad2);
+
+            // Store the gamepad values from this loop iteration in currentGamepad1/2 to be
+            // used for this loop iteration. This prevents the gamepad values from changing
+            // between being used and stored in previousGamepad1/2.
+            currentGamepad1.copy(gamepad1);
+            currentGamepad2.copy(gamepad2);
+
+            // Update the drive powers based on gamepad 1
+            updateDrivePowers(currentGamepad1);
 
         }
 
